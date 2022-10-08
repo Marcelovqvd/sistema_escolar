76b9e854b8e20f142f8fb2ff5949971c4c7e5ebb
Marcelo <marcelovqvd@gmail.com>
2022-10-08 17:39:07 -0300

    feat: vite + linting

--- app/src/modules/students/entities/Students.ts
+++ app/src/modules/students/entities/Students.ts
@@ -1,32 +1,32 @@
 interface StudentsProps {
   name: string
-  age: number
+	age: number
   document: number
 }
 
 export class Student {
-  private props: StudentsProps;
+	private props: StudentsProps
 
-  get student() {
-    return this.props.name;
-  }
-  
-  set age(age: number) {
-    if (this.props.age > 17) {
-      throw new Error('Não é permitida a matrícula de alunos com idade acima de 17 anos')
-    }
-    this.props.age = age
-  }
-  
-  get age() {
-    return this.props.age
-  }
+	get student() {
+		return this.props.name
+	}
 
-  get document() {
-    return this.props.document
-  }
+	set age(age: number) {
+		if (this.props.age > 17) {
+			throw new Error('Não é permitida a matrícula de alunos com idade acima de 17 anos')
+		}
+		this.props.age = age
+	}
 
-  constructor(props: StudentsProps) {
-    this.props = props
-  }
+	get age() {
+		return this.props.age
+	}
+
+	get document() {
+		return this.props.document
+	}
+
+	constructor(props: StudentsProps) {
+		this.props = props
+	}
 }
