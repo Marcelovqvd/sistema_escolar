30a97aa0c7a71f962c69d6fdf74374d98e7cddc1
Marcelo <marcelovqvd@gmail.com>
2022-10-15 00:14:27 -0300

    Entities

--- app/src/domain/entities/Students.ts
+++ app/src/domain/entities/Students.ts
@@ -1,3 +1,18 @@
-export class Subjects {
-  constructor() { }
+import { Entity } from "core/domain/Entity";
+
+type StudentsProps = {
+  name: string;
+  document: string;
+};
+
+export class Students extends Entity<StudentsProps> {
+  private constructor(props: StudentsProps, id?: string) {
+    super(props, id);
+  }
+
+  create(props: StudentsProps, id?: string) {
+    const student = new Students(props, id);
+
+    return student;
+  }
 }
