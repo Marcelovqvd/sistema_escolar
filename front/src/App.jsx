69063f75388bdebb04930303895f87db0a7262b7
Marcelo <marcelovqvd@gmail.com>
2022-12-25 20:00:27 -0300

    sidebar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,29 @@
-import "./global.css"
+import { Header } from './components/Header';
+import { Post } from './components/Post'
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
-
   return (
-    <div className="App">
-      <h2>Sistema Escolar</h2>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
